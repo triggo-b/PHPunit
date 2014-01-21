73628cce797562c78e0f413d325a6760f55bf9c5
bo <tigerz@mail.ru>
2014-01-21 17:13:50 +0400

    1

---
+++ lib/Project/Foo.php
@@ -0,0 +1,11 @@
+<?php
+namespace Project;
+
+class Foo
+{
+	public function Math($a, $b)
+	{
+		$Ex = $a / $b;
+		return $Ex;
+	}
+}
---
+++ test/FooTest.php
@@ -0,0 +1,11 @@
+<?php
+use Project\Foo as Foo;
+
+class FooTest extends \PHPUnit_Framework_TestCase
+{
+	public function testZero()
+	{
+		$my = new Foo();
+		$this->assertEquals(7, $my->Math(9, 3));
+	}
+}
