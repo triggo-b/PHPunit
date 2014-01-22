75df2d7bd454bff09268140ee43d6ee97eec805c
bo <tigerz@mail.ru>
2014-01-22 11:46:09 +0400

    add func

--- lib/Project/Foo.php
+++ lib/Project/Foo.php
@@ -8,9 +8,6 @@ class Foo
         if(is_int($b)) {
             throw new Exception\Alarm('Integer');
         }
-        if(is_bool($b)) {
-            throw new Exception\Alarm('Boolean');
-        }
 		$Ex = $a / $b;
 		return $Ex;
 	}
