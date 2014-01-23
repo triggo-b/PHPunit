6c78b73dffb8367620cd77c815dbaa784b36b5eb
bo <tigerz@mail.ru>
2014-01-23 15:54:04 +0400

    setUp, tearDown, 100% coverage

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -4,29 +4,47 @@ use Weather\Client as Client;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
-//    public function testStub()
-//    {
-//        $mock = $this->getMock('Client');
-//
-//        $mock->expects($this->once())
-//             ->method('getWeatherByCity')
-//             ->will($this->returnValue('{"data": [4,5,6]}'));
-//
-//        $this->assertInstanceOf('Weather\Client', $mock);
-//    }
-
-
-    public function testSt()
+    public function testInstanse()
     {
-        $x = new Client();
-        $mock = $this->getMock('Buzz');
-        $mock->expects($this->once())
-             ->method('getWeatherByCity')
-             ->will($this->returnValue('{"data": [4,5,6]}'))
-             ->with(1, $this->isInstanceOf('Weather\Response'));
-        $x->getWeatherByCity($mock);
+        $mock = $this->getMock("Buzz\\Browser");
+        $mock->expects($this->any())
+             ->method("getWeatherByCity")
+             ->will($this->returnValue('{"data": [4,5,6]}'));
 
-//        $this->assertInstanceOf('Weather\Client', $mock);
+        $dp = new Client($mock);
+        $this->assertInstanceOf('Weather\Response', $dp->getWeatherByCity());
+        $this->assertInternalType('object', $dp->getWeatherByCity());
     }
 
+    protected $fixture;
+
+    protected function setUp()
+    {
+        $this->fixture = new \Weather\Response('{"coord":{"lon":39.17,"lat":51.67},"sys":{"message":0.103,"country":"RU","sunrise":1390454036,"sunset":1390485393},"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],"base":"gdps stations","main":{"temp":262.467,"temp_min":262.467,"temp_max":262.467,"pressure":1006.41,"sea_level":1024.79,"grnd_level":1006.41,"humidity":89},"wind":{"speed":5.83,"deg":49.5},"snow":{"3h":0},"clouds":{"all":92},"dt":1390469285,"id":472045,"name":"Voronezh","cod":200}');
+    }
+
+    protected function tearDown()
+    {
+        $this->fixture = NULL;
+    }
+
+    public function testPressure()
+    {
+        $this->assertEquals(1002, $this->fixture->getPressure());
+    }
+
+    public function testTemperature()
+    {
+        $this->assertEquals(16, $this->fixture->getTemperature());
+    }
+
+    public function testWindSpeed()
+    {
+        $this->assertEquals(3.5, $this->fixture->getWindSpeed());
+    }
+    
+    public function testCity()
+    {
+        $this->assertEquals('Voronezh', $this->fixture->getCity());
+    }
 }
